aa0574668a789e8346d4afbd668010cd74dde1d0
Milouyg <33333@ma-web.nl>
2022-10-09 21:14:42 +0200

    hamburger menu gemaakt voor mobile

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-// Deze functie verwijst naar de sign up pagina
+// This function refers to the signup page
+
 function signUp(){
     window.location.href = "http://127.0.0.1:5500/signUp.html";
+}
+
+// This function open menubar on mobile
+const button = document.getElementById("js--openMenu");
+const addMenu = document.getElementsByClassName("header__nav")[0];
+button.onclick = function(){ 
+    if(addMenu.classList.contains("header__nav--openMenu")){ 
+        addMenu.classList.remove("header__nav--openMenu");
+    }
+    else{
+        addMenu.classList.add("header__nav--openMenu");
+    }
 }
 
