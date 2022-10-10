3ea2e4287572615b3beb8ee42e2366351e83ae4b
Milouyg <33333@ma-web.nl>
2022-10-10 20:55:43 +0200

    login page created

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 // This function refers to the signup page
 
 function signUp(){
-    window.location.href = "http://127.0.0.1:5500/signUp.html";
+    document.getElementById("js--signUp").style.display = "block";
 }
+
+function closeSignUp(){
+    document.getElementById("js--signUp").style.display = "none";
+}
+
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+let formButton = document.getElementById("btn");
+
+        function register(){
+            x.style.left = "-40rem";
+            y.style.left = "5rem";
+            formButton.style.left = "11rem";
+        }
+
+        function login(){
+            x.style.left = "5rem";
+            y.style.left = "45rem";
+            formButton.style.left = "0rem";
+        }
+
+
 
 // This function open menubar on mobile
 const button = document.getElementById("js--openMenu");
@@ -15,4 +37,5 @@ button.onclick = function(){
         addMenu.classList.add("header__nav--openMenu");
     }
 }
+
 
